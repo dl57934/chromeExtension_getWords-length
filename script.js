6c4fdea521dff78aad481177825c539ddfda0225
LeeSangHoon <dl57934@naver.com>
2018-04-09 02:11:48 +0900

    user chrome storage

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-document.querySelector("#insertZone").addEventListener("change",function () {
+function matching(insertData) {
     chrome.tabs.executeScript({
         code:'document.querySelector("body").innerText;'
     },function (result) {
         var bodyText = result[0];
         var bodynum =bodyText.split(' ').length;
-        var insertData = $('#insertZone').val();
         var mynum = bodyText.match(new RegExp("\\b("+insertData+")\\b","gi")).length;
-        var per = mynum/bodynum*100;
-        per = per.toFixed(1);
+        if (mynum != null) {
+            var per = mynum / bodynum * 100;
+            per = per.toFixed(1);
+            document.getElementById("result").innerText = mynum + " / " + bodynum + "= " + per + "%";
+        }else{
+            document.getElementById("result").innerText = "0%";
+        }
 
-        document.getElementById("result").innerText = mynum+" / "+bodynum +"= "+ per+"%" ;
 
     });
+}
+
+chrome.storage.sync.get(function(data){
+   document.querySelector("#insertZone").value = data.userWords;
+   matching(data.userWords);
+});
+document.querySelector("#insertZone").addEventListener("change",function () {
+    var insertData = document.querySelector("#insertZone").value;
+    chrome.storage.sync.set({
+       userWords:insertData
+    });
+    matching(insertData);
 });
 
 
