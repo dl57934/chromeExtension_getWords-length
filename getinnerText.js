b1372f09166d933d16c5b4914ffab951850a1905
leesanghoon <dl57934@naver.com>
2018-04-08 23:34:53 +0900

    add extensionProgram at chrome

--- getinnerText.js
+++ getinnerText.js
@@ -6,7 +6,11 @@ var url = "https://en.wikipedia.org/wiki/Gundam";
 request(url,function (err,res,html) {
     var $ = cheerio.load(html);
     var bodyData = $.text();
+    console.log(bodyData)
     var bodyNum = bodyData.split(' ').length;
-    console.log(bodyData.match(new RegExp("\\bthe\\b","gi")).length);
+    var mynum = bodyData.match(new RegExp('\\b(gundam)\\b',"gi")).length;
+    console.log(mynum)
+    var percent = mynum/bodyNum;
+    console.log(percent*100+"%");
 })
 
