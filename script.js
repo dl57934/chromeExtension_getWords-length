c542374e44e2c7f96d4733f0e0cc44c4eefcccdf
leesanghoon <dl57934@naver.com>
2018-04-09 01:21:06 +0900

    add words result complete but yet use chrome storage

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+document.querySelector("#insertZone").addEventListener("change",function () {
+    chrome.tabs.executeScript({
+        code:'document.querySelector("body").innerText;'
+    },function (result) {
+        var bodyText = result[0];
+        var bodynum =bodyText.split(' ').length;
+        var insertData = $('#insertZone').val();
+        var mynum = bodyText.match(new RegExp("\\b("+insertData+")\\b","gi")).length;
+        var per = mynum/bodynum*100;
+        per = per.toFixed(1);
 
-chrome.tabs.executeScript({
-    code:'document.querySelector("body").innerText;'
-},function (result) {
-   var bodyText = result[0];
-   var bodynum =bodyText.split(' ').length;
-   var mynum = bodyText.match(new RegExp("\\b(the|a)\\b","gi")).length;
-   var per = mynum/bodynum*100;
-   per = per.toFixed(1);
-    document.getElementById("result").innerText = mynum+" / "+bodynum +"= "+ per+"%"
+        document.getElementById("result").innerText = mynum+" / "+bodynum +"= "+ per+"%" ;
+
+    });
 });
+
+
+
+
+
+
+
