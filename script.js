f55c675da00536f50f7a1d2fb8c1d914c0ea5cf8
LeeSangHoon <dl57934@naver.com>
2018-04-09 22:24:00 +0900

    add Korean, 0%

--- script.js
+++ script.js
@@ -2,21 +2,29 @@ function matching(insertData) {
     chrome.tabs.executeScript({
         code:'document.querySelector("body").innerText;'
     },function (result) {
+        document.getElementById("result").innerText = "0%";
         var bodyText = result[0];
         var bodynum =bodyText.split(' ').length;
-        var mynum = bodyText.match(new RegExp("\\b("+insertData+")\\b","gi")).length;
-        if (mynum != null) {
-            var per = mynum / bodynum * 100;
-            per = per.toFixed(1);
-            document.getElementById("result").innerText = mynum + " / " + bodynum + "= " + per + "%";
-        }else{
-            document.getElementById("result").innerText = "0%";
+
+        var check = /[가-힣]/;
+        if(check.test(insertData)) {
+            alert(insertData);
+            
+            var mynum = bodyText.match(new RegExp(insertData,"gi")).length;
+            getResult(mynum,bodynum);
+        }else {
+            var mynum = bodyText.match(new RegExp("\\b("+insertData +")\\b","gi")).length;
+            getResult(mynum,bodynum);
         }
-
-
     });
+
 }
 
+function getResult(mynum,bodynum) {
+    var per = mynum / bodynum * 100;
+    per = per.toFixed(1);
+    document.getElementById("result").innerText = mynum + " / " + bodynum + "= " + per + "%";
+}
 chrome.storage.sync.get(function(data){
    document.querySelector("#insertZone").value = data.userWords;
    matching(data.userWords);
